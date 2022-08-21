6f62547407aa3681b07585297bb77125ec8c9f06
seraphinush <seraphinus.hong@gmail.com>
2022-08-21 17:29:50 +0900

    Fixed copy url

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,4 @@ export default {
   axios: {
     baseURL: process.env.API_BASE_URL || "http://localhost:4000/",
   },
-  publicRuntimeConfig: {
-    APP_BASE_URL: process.env.APP_BASE_URL || "http://localhost:3000/",
-  },
 };
