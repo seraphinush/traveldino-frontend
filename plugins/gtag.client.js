1f47b48b30e3eb26e279be2124d58759d0f9acaf
seraphinush <seraphinus.hong@gmail.com>
2024-01-15 13:32:49 +0900

    Fixed optional GGL_GTAG_ID issue

--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -1,8 +1,10 @@
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
   const { GGL_GTAG_ID } = config.public;
-  if (!GGL_GTAG_ID)
-    throw new Error("Environmental variable GGL_GTAG_ID is undefined");
+  if (!GGL_GTAG_ID) {
+    // throw new Error("Environmental variable GGL_GTAG_ID is undefined");
+    return console.error("Environmental variable GGL_GTAG_ID is undefined");
+  }
 
   function gtag() {
     window.dataLayer.push(arguments);
