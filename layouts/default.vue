4388a12c9435c2e543bf8f5adab45ac28ba79bef
seraphinush <seraphinus.hong@gmail.com>
2024-01-28 11:21:25 +0900

    Updated useSeoMeta with ogUrl

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,5 +11,6 @@ useSeoMeta({
     "전 세계를 돌아다닌 다이노의 경험을 담아 새로운 여행지를 추천해요!",
   ogDescription:
     "전 세계를 돌아다닌 다이노의 경험을 담아 새로운 여행지를 추천해요!",
+  ogUrl: process.env.WEB_BASE_URL || undefined,
 });
 </script>
